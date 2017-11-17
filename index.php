719af7be941c0a2ed0623d34fbb17e37e48f1210
Luis Salazar Alvarez <anibalps042@hotmail.com>
2017-11-17 14:37:44 -0500

    Programa en php

---
+++ index.php
@@ -0,0 +1,20 @@
+<html>
+	<head>
+		<title>Practica con PHP</title>
+	</head>
+
+	<body>
+		<h1>Programa que suma</h1>
+
+		<input type="number" name="numero1">
+		<?php  
+
+		$resultado=$numero1+$numero2;
+			
+		echo "El resultado de ".$numero1."+".$numero2." es = ".$numero1;
+
+
+		?>
+		
+	</body>
+</html>
